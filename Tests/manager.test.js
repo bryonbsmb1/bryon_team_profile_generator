332ec289ea98b03f22048aa96dd78bf53028fd99
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-17 19:22:51 -0500

    updated error in lib file now running

--- Tests/manager.test.js
+++ Tests/manager.test.js
@@ -3,14 +3,14 @@ const Manager = require('../lib/Manager');
 
 // creating manager object  
 test('creates an Manager object', () => {
-    const manager = new Manager('Tom', 90, 'tstacey@fbinsmi.com', 4);
+    const manager = new Manager('Tom', 90, 'tstacey@noemail.com', 4);
     
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Tom', 90, 'tstacey@fbinsmi.com.com');
+    const manager = new Manager('Tom', 90, 'tstacey@noemail.com');
 
     expect(manager.getRole()).toEqual("Manager");
 }); 
